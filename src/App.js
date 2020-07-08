331d98b9440a710be28ad1d9d506d9d28580c7f5
rohanJa <rohan.jain150300@gmail.com>
2020-07-08 18:10:23 +0530

    Updated main App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
-
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -15,7 +16,11 @@ const  App = () => {
   return (
     <Fragment>
       <SearchBar />
-      <Logs />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
+      </div>
     </Fragment>
   );
 }
